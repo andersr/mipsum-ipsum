60298c65f674fd03cfd87e0905929cbe50990e4d
Anders Ramsay <andersr@gmail.com>
2016-08-30 08:31:37 -0400

    shuffle words

---
+++ libs/latin_words.js
@@ -0,0 +1,21 @@
+//USE CSV FORMAT INSTEAD EVENTUALLY
+module.exports = [
+  'ac',
+  'accumsan',
+  'adipiscing',
+  'consectetur',
+  'elit',
+  'eros',
+  'iaculis',
+  'ipsum',
+  'libero',
+  'lorem',
+  'luctus',
+  'placerat',
+  'sem',
+  'tristique',
+  'ullamcorper',
+  'ut',
+  'vehicula',
+  'vestibulum'
+]
---
+++ libs/lipsum.js
@@ -0,0 +1,6 @@
+var shuffle = require('shuffle-array')
+var words = require('./latin_words')
+
+var shuffledWords = shuffle(words)
+
+// console.log(shuffledWords)
