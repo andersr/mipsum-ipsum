5cc26366c4464e44e2864557b103284fab343b61
Anders Ramsay <andersr@gmail.com>
2016-08-31 11:04:35 -0400

    refactor make paragraph

--- libs/generate_lipsum.js
+++ libs/generate_lipsum.js
@@ -5,28 +5,36 @@ import { capitalize, randomNumberBetween, shuffleItems } from './utils'
 // generate a single text block with a min/max range.
 
 export default function randomWordParagraph (srcWords) {
-  function createSentence (wordQty) {
-    const words = []
-    const shuffledWords = shuffleItems(srcWords)
+  const totalWords = randomNumberBetween(20, 25)
+  const maxWordLength = 8
+  const wordCollection = shuffleItems(srcWords).slice(0, totalWords)
+  let wordQty
 
-    for (let i = 0; i < wordQty; i++) {
-      words.push(shuffledWords[i])
+  return makeParagraph()
+
+  // console.log('newParagraph, ', newParagraph)
+  //
+  // return newParagraph
+
+  function makeParagraph () {
+    let paragraph = []
+    let wordsUsed = 0
+    let sentenceWordQty
+
+    while (wordsUsed < totalWords) {
+      sentenceWordQty = randomNumberBetween(3, 8)
+      paragraph.push(makeSentence(wordsUsed, sentenceWordQty))
+      wordsUsed += sentenceWordQty
     }
-    var sentence = words.join(' ')
-    sentence += '.'
-    return capitalize(sentence)
+    return paragraph.join(' ')
   }
 
-  function createParagraph () {
-    const sentenceQty = randomNumberBetween(2, 4)
-    const textBlock = []
-    let wordQty
-    for (let i = 0; i < sentenceQty; i++) {
-      wordQty = randomNumberBetween(3, 7)
-      textBlock.push(createSentence(wordQty))
-    }
+  function makeSentence (start, length) {
+    const end = start + length
+    let sentence = wordCollection.slice(start, end)
+    sentence[sentence.length - 1] += '.'
+    sentence[0] = capitalize(sentence[0])
 
-    return textBlock.join(' ')
+    return sentence.join(' ')
   }
-  return createParagraph()
 }
