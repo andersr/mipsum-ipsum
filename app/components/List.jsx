01f4d7c8c2078cd49149b5b84cc547d0d9e0467e
Anders Ramsay <andersr@gmail.com>
2016-09-01 14:36:23 -0400

    add no-bullets list-style

--- app/components/List.jsx
+++ app/components/List.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const List = props => <ul>{props.listItems.map((item, key) => <li key={key}>{item}</li>)}</ul>
+const List = props => <ul className={props.style}>{props.listItems.map((item, key) => <li key={key}>{item}</li>)}</ul>
 
 export default List
 
 List.propTypes = {
-  listItems: React.PropTypes.array.isRequired
+  listItems: React.PropTypes.array.isRequired,
+  style: React.PropTypes.string
+}
+
+List.defaultProps = {
+  style: 'list-no-bullets'
 }
