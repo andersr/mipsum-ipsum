00d4bfb0c5bc8662ddf853623bacac8837a25b73
Anders Ramsay <andersr@gmail.com>
2016-09-03 09:22:00 -0400

    fixed app header

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import AppHeader from './AppHeader'
-import TextHeading from './TextHeading'
 import LipsumListContainer from '../containers/LipsumListContainer'
 import STATIC_TEXT from '../../libs/static_text'
 import LATIN_WORDS from '../../libs/latin_words'
-// import Octicon from 'react-octicon'
 
 export default class App extends React.Component {
   constructor (props) {
     super(props)
   }
 
-  render() {
+  render () {
     return (
       <div id='app-container'>
         <AppHeader title={STATIC_TEXT.appInfo.title} tagline={STATIC_TEXT.appInfo.tagline} />
+        <div id='main-content'>
         <LipsumListContainer lipsumData={LATIN_WORDS} />
+        </div>
       </div>
     )
   }
--- app/components/AppHeader.jsx
+++ app/components/AppHeader.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import TextHeading from './TextHeading'
 import Paragraph from './Paragraph'
 
-const AppHeader = props =>
-  <div className='flex-row flex-vertical-baseline'>
-    <div className='flex-main-content'><TextHeading text={props.title} /></div>
-    <Paragraph text={props.tagline} style='secondary-text' />
-	</div>
+const AppHeader = props => {
+  return (
+    <header className='fixed-header'>
+      <div className='flex-row flex-vertical-baseline'>
+        <div className='flex-main-content'>
+          <TextHeading text={props.title} />
+        </div>
+        <Paragraph text={props.tagline} style='secondary-text' />
+        </div>
+      <div className='flex-row flex-centered'>
+        Content
+      </div>
+   </header>
+  )
+}
 
 export default AppHeader
 
