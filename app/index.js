2ab521d604f2eb46e7efdd434b3d4a873426d5ec
Anders Ramsay <andersr@gmail.com>
2016-09-05 08:43:31 -0400

    rm redux from entry point

--- app/index.js
+++ app/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import lipsumApp from './reducers'
+// import { Provider } from 'react-redux'
+// import { createStore } from 'redux'
+// import lipsumApp from './reducers'
 import AppContainer from './containers/AppContainer'
 import LATIN_WORDS from '../libs/latin_words'
 
 const STATIC_CONTENT = require('../libs/static_content')
 
-let store = createStore(lipsumApp)
+// let store = createStore(lipsumApp)
 
-ReactDOM.render(
-  <Provider store={store}>
-  <AppContainer staticContent={STATIC_CONTENT} sourceWords={LATIN_WORDS} />
-  </Provider>
-  , document.getElementById('app'))
+// ReactDOM.render(
+//   <Provider store={store}>
+//   <AppContainer staticContent={STATIC_CONTENT} sourceWords={LATIN_WORDS} />
+//   </Provider>
+//   , document.getElementById('app'))
+
+ReactDOM.render(<AppContainer staticContent={STATIC_CONTENT} sourceWords={LATIN_WORDS} />, document.getElementById('app'))
